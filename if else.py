ef0ff2046ace6ba5f51bcc8d92f228a41838f762
Kvsandhu <56722545+Kvsandhu@users.noreply.github.com>
2020-01-29 13:23:32 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,8 @@
+a=int(input('Enter the first number '))
+b=int(input('Enter the second number '))
+if(a>b):
+    print("{} is greater then {}".format(a,b))
+elif(a==b):
+        print("{} Both {} Values are Same".format(a,b))
+else:
+            print("{} is Greater then {}".format(b,a))
---
+++ thyrom.py
@@ -0,0 +1,5 @@
+import math
+a=float(input('Enter the first number '))
+b=float(input('Enter the second number '))
+c=math.sqrt(a**2 + b**2)
+print("value of c:",c)
